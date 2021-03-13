323efc8d90d305f5abbe08c20482f79bdcc5bbd5
Cosimo <outis@manjaro.lan>
2021-03-13 16:12:12 +0100

    adding exercise_0.js

--- Lab 1/exercise_0.js
+++ Lab 1/exercise_0.js
@@ -1,3 +1,25 @@
 "use strict";
 
-let a = 0;
+let array = ["alessandro", "cosimo", "katia", "giuseppe", "or" ];
+
+const del = function(array){
+    array.forEach(element => {
+        if (element.length < 3){
+            array[array.indexOf(element)] = "";
+        }
+        else{
+            let firstpart = element.substr(0,2);
+            let lastpart = element.substr(element.length-2, element.length-1);
+            let final = firstpart.concat(lastpart);
+            array[array.indexOf(element)] = final;
+        }
+    });
+}
+
+del(array);
+
+console.log(array);
+
+
+
+
